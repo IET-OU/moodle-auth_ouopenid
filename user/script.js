5726785deb48345f966e88ca1b9ea9ca5433e660
Nick Freear <nfreear@yahoo.co.uk>
2017-03-14 15:46:13 +0000

    Make `user/script` Javascript more robust with `when_call` [iet:8336249]
    * Plus, fix `eslint` composer script

--- user/script.js
+++ user/script.js
@@ -2,36 +2,40 @@
   OU-OpenID. Nick Freear / © The Open University.
 */
 
-//window.console.debug('auth/ouopenid/script.js');
-
-window.setTimeout(function () {
+(function (W) {
 
   var user_json = '/auth/ouopenid/user/ajax.php?r=' + rand()
-    , require = window.require
-    , C = window.console
-    , L = window.location;
+    , C = W.console
+    , L = W.location;
 
   if (L.pathname.match(/^\/admin\//)) {
     return C.debug('ouopenid: admin page, exiting.');
   }
 
 
-  require([ 'jquery' ], function ($) {
+  when_call(function () {
+    return W.require;
+  },
+  function () {
+    var require = W.require;
 
-    C.debug('ouopenid $:', $.fn.jquery);
+    require([ 'jquery' ], function ($) {
 
-    $.getJSON(user_json).done(function (data, textStat, jqXHR) {
+      C.debug('ouopenid $:', $.fn.jquery);
 
-      C.debug('ouopenid JSON: ', data);
+      $.getJSON(user_json).done(function (data, textStat, jqXHR) {
 
-      $('body').addClass(data.body_class);
-      //document.body.className += ' ' + data.body_class;
+        C.debug('ouopenid JSON: ', data, jqXHR);
 
-    }).fail(function (jqXHR, textStat, ex) {
-      C.error('ouopenid error: ', textStat, jqXHR, ex);
+        $('body').addClass(data.body_class);
 
-      $('body').addClass('ouop-ouopenid-error');
-    });
+      }).fail(function (jqXHR, textStat, ex) {
+        C.error('ouopenid error: ', textStat, jqXHR, ex);
+
+        $('body').addClass('ouop-ouopenid-error');
+      });
+
+    }); //End require.
 
   });
 
@@ -41,5 +45,15 @@ window.setTimeout(function () {
     return Math.floor(Math.random() * (max - min)) + min;
   }
 
+  // https://gist.github.com/nfreear/f40470e1aec63f442a8a
+  function when_call(when_true_FN, callback_FN, interval) {
+    var int_id = W.setInterval(function () {
+      if (when_true_FN()) {
+        W.clearInterval(int_id);
+        callback_FN();
+      }
+    }, interval || 200); // Milliseconds.
+  }
 
-}, 3000);
+
+}(window));
